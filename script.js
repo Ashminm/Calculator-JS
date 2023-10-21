d7c79b6809d14e2271ea86951053e2a9917ba5b3
Ashmin <ashmin2802@gmail.com>
2023-10-21 15:13:10 +0530

    Done

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+// ------ operation display view ----------
 function numClick(val){
     document.querySelector("#outScreen").value+=val;
 }
 
+
+//  -------- Clear button (disClear()) ----------------
 function disClear(){
     document.querySelector("#outScreen").value ="";
 }
 
+
+    // -------- delete button (backClick()) --------------
 function backClick(){
     document.querySelector("#outScreen").value =document.querySelector("#outScreen").value.toString().slice(0,-1);
 }
 
+
+//  ------------- equal button (equal()) ------------------
 function equal(){
     var text=document.querySelector("#outScreen").value;
     var res=eval(text);
